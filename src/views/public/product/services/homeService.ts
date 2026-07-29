858f2ed065e41828c413af3627d2e789d62bc6e1
denilsonali2020 <denilsontrochez32@gmail.com>
2026-07-29 10:25:48 -0600

    primer avance en busqueda de un producto

---
+++ src/views/public/product/services/homeService.ts
@@ -0,0 +1,20 @@
+import api from "@/lib/axios";
+
+import { isAxiosError } from "axios";
+
+export async function navigation() {
+  try {
+    const url = "/home/navigation";
+    const { data } = await api.get(url);
+    // const response = navigationSchema.safeParse(data);
+    // if (!response.success) {
+    //   throw new Error("Error al cargar los datos");
+    // }
+    // return response.data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error || "Hubo un error");
+    }
+    throw new Error("Hubo un error inesperado");
+  }
+}
