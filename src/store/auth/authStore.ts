4a52c37247d3d746d784679b1a916fa5f63fd4e7
denilsonali2020 <denilsontrochez32@gmail.com>
2026-07-30 19:21:30 -0600

    mejorando la navegacion de una pestaña a otra y manteniendo la sesion en produccion

--- src/store/auth/authStore.ts
+++ src/store/auth/authStore.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { devtools } from "zustand/middleware";
+import { devtools, persist } from "zustand/middleware";
 
 // Define the User type
 type User = {
@@ -23,24 +23,31 @@ type AuthState = {
 
 // Create the auth store using Zustand
 export const useAuthStore = create<AuthState>()(
-  devtools((set) => ({
-    token: null,
-    user: null,
-    isAuth: false,
-    setAuth: (token, user) => set({ token, user, isAuth: true }),
-    setName: (name) =>
-      set((state) => ({
-        user: state.user ? { ...state.user, name } : null,
-      })),
-    logout: () => set({ token: null, user: null, isAuth: false }),
-    mustChangePassword: () =>
-      set((state) => {
-        if (state.user) {
-          return {
-            user: { ...state.user, mustChangePassword: false },
-          };
-        }
-        return {};
+  devtools(
+    persist(
+      (set) => ({
+        token: null,
+        user: null,
+        isAuth: false,
+        setAuth: (token, user) => set({ token, user, isAuth: true }),
+        setName: (name) =>
+          set((state) => ({
+            user: state.user ? { ...state.user, name } : null,
+          })),
+        logout: () => set({ token: null, user: null, isAuth: false }),
+        mustChangePassword: () =>
+          set((state) => {
+            if (state.user) {
+              return {
+                user: { ...state.user, mustChangePassword: false },
+              };
+            }
+            return {};
+          }),
       }),
-  })),
+      {
+        name: "auth-storage",
+      },
+    ),
+  ),
 );
